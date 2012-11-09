6ccb8add45f504b442d6c874b55b8038797daae0
Jason McEwen <jason.mcewen@ucl.ac.uk>
2012-11-09 11:12:01 +0000

    Added c documentation to trunk.

---
+++ doc/c/search/defines_6d.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['max',['MAX',['../flaglet_8h.html#afa99ec4acc4ecb2dc3c2d05da15d0e3f',1,'flaglet.h']]],
+  ['min',['MIN',['../flaglet_8h.html#a3acffbd305ee72dcd4593c0d8af64a4f',1,'flaglet.h']]]
+];
