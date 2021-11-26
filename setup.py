4f09837c5cebabfbef3d5d8eab927bf9e454a1fa
CosmoMatt <matt.price@kagenova.com>
2021-11-26 15:00:58 +0000

    update python extension, C workflow, and configure for pip deployment

---
+++ setup.py
@@ -0,0 +1,40 @@
+from pathlib import Path 
+from skbuild import setup
+
+cmake_args = ["-Dtests:BOOL=OFF"]
+
+long_description = (
+    Path(__file__).parent / "src" / "main" / "pyflaglet" / ".FLAGLET_Python_Documentation.rst"
+).read_text()
+
+setup(
+    name="pyflaglet",
+    version="0.0.1",
+    author="Boris Leistedt & Jason McEwen",
+    install_requires=["numpy", "cython", "scipy"],
+    extras_require={
+        "dev": [
+            "numpy"
+            "setuptools",
+            "wheel",
+            "scikit-build",
+            "cmake",
+            "ninja",
+            "cython",
+            "conan",
+            "black",
+            "pytest-black"
+            "pytest",
+        ]
+    },
+    description="Fast wavelet transform on the ball",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/astro-informatics/flaglet",
+    package_dir={"pyflaglet": "src/main/pyflaglet"},
+    package_data={"pyflaglet": ["FLAGLET_Python_Documentation.rst"]},
+    cmake_args=cmake_args,
+    cmake_languages=("C",),
+    license="GPL-3",
+    packages=["pyflaglet"],
+)
